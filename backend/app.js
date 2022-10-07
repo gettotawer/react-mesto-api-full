9bea7525e770345228ff9256eba7cae6358784f2
Maxim K <max.2505@yandex.ru>
2022-10-08 01:59:20 +0300

    cors5

--- backend/app.js
+++ backend/app.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-    // return res.end();
+    return res.status(200);
   }
 
   return next();
